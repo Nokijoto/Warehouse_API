09bceb88464a2423d0d1b8091782a3d2a1115ae6
Rafał Wątroba <rafalwatroba@protonmail.com>
2024-06-21 19:17:05 +0200

    Authentication
    Add Basic Auth

--- Warehouse_API/Controllers/WeatherForecastController.cs
+++ Warehouse_API/Controllers/WeatherForecastController.cs
@@ -1,9 +1,10 @@
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Warehouse_API.Controllers
 {
     [ApiController]
-    [Route("[controller]")]
+    [Route("api/[controller]")]
     public class WeatherForecastController : ControllerBase
     {
         private static readonly string[] Summaries = new[]
@@ -18,7 +19,7 @@ namespace Warehouse_API.Controllers
             _logger = logger;
         }
 
-        [HttpGet(Name = "GetWeatherForecast")]
+        [HttpGet(Name = "GetWeatherForecast"),Authorize]
         public IEnumerable<WeatherForecast> Get()
         {
             return Enumerable.Range(1, 5).Select(index => new WeatherForecast
