7f3d51a409a3b8419dcc879ac5012f54ee689cb1
Rafał Wątroba <rafalwatroba@protonmail.com>
2024-06-21 20:39:12 +0200

    Add Products
    Add Products

---
+++ Warehouse_API/Dto/ProductDTO.cs
@@ -0,0 +1,12 @@
+﻿using Common.Dto;
+
+namespace Warehouse_API.Dto
+{
+    public class ProductDTO: BaseDto
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public decimal Price { get; set; }
+        public int Stock { get; set; }
+    }
+}
---
+++ Warehouse_API/Dto/RFIDTagDTO.cs
@@ -0,0 +1,9 @@
+﻿using Common.Entities;
+
+namespace Warehouse_API.Dto
+{
+    public class RFIDTagDTO :BaseEntity
+    {
+        public string TagNumber { get; set; }
+    }
+}
