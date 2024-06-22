dbf4894303aa1b1774e8546c92a078e5de3c5705
Rafał Wątroba <rafalwatroba@protonmail.com>
2024-06-22 12:38:47 +0200

    Add Database Connection
    And First Migration

---
+++ Warehouse_API/WarehouseDbContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Warehouse_API.Entities;
+
+namespace Warehouse_API
+{
+    public class WarehouseDbContext : DbContext
+    {
+        DbSet<Product> Products { get; set; }
+
+        public WarehouseDbContext(DbContextOptions<WarehouseDbContext> options) : base(options)
+        {
+        }
+
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+         
+        }
+    }
+}
