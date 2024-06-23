b25526f5e612245bfdc745e990250c3136c288d6
Rafał Wątroba <rafalwatroba@protonmail.com>
2024-06-23 11:19:36 +0200

    Simple Raports Added
    Added Simple Raport

---
+++ Warehouse_API/Extensions/Dtos/RaportDtoExtension.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Warehouse_API.Dto;
+using Warehouse_API.Dto.CreationsDto;
+using Warehouse_API.Entities;
+
+namespace Warehouse_API.Extensions.Dtos
+{
+    public static class RaportDtoExtension
+    {
+        public static Product ToEntity(this RaportDto product)
+        {
+            return new Product
+            {
+                Name = product.Name,
+                Description = product.Description,
+                Price = product.Price
+            };
+        }
+    }
+      
+}
